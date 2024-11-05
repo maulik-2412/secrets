9c2c6674c051b676a81c19a0d9615f6d486c37c6
Maulik Tyagi <mauliktyagi08@gmail.com>
2024-11-05 22:20:03 +0530

    provided better logout mgmt

--- app.js
+++ app.js
@@ -114,6 +114,13 @@ app.route("/logout")
             if(err){
                 alert("alert logging out");
             }
+        });
+            req.session.destroy((err) => {
+                if (err) {
+                    console.error("Error destroying session:", err);
+                    return res.status(500).send("Error logging out");
+                }
+            res.clearCookie('connect.sid');
             res.redirect("/");
         });
         
