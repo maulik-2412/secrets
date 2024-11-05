2faded61270b88ca2a1aeb189af5403c1634d956
Maulik Tyagi <mauliktyagi08@gmail.com>
2024-11-05 21:42:40 +0530

    corrected

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: process.env.CALLBACK_URL
   },
   function(accessToken, refreshToken, profile, cb) {
     Users.findOrCreate({ googleId: profile.id }, function (err, user) {
